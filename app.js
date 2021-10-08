50f430fe36cbc9d0307d799e7508b56050b4c7c4
Philippe Ngom <philngom@gmail.com>
2021-10-08 15:01:18 -0700

    button working to display text

--- app.js
+++ app.js
@@ -4,17 +4,36 @@
 const blanks = [];
 
 const buttonPress = document.getElementById('button');
+const answers = [];
 
 buttonPress.addEventListener('click', () => {
+    for (let i = 1; i <= 13; i++) {
+        // let blankValue = blanks[i].value;
+        blanks[i] = document.getElementById(`blank-${i}`);
+        answers[i] = blanks[i].value;
+    }
+    let text = `We are ${answers[1]} to inform you that you have been accepted at
+    Hogwarts School of ${answers[2]} and ${answers[3]}.
+    Please find enclosed a list of all necessary books and equipment.
+    Term begins on 1 September. We await your owl by no later than 31 July.
 
-for (let i = 1; i <= 13; i++) {
-    
-    blanks[i] = document.getElementById(`blank-${i}`.value);
-}
+    UNIFORM
+    First-year students will require:
+    1. Three sets of ${answers[4]} ${answers[5]}
+    2. One ${answers[6]} ${answers[7]} ${answers[8]} for day wear
+    3. One pair of ${answers[9]} gloves (${answers[10]} hide or similar)
+    Please note that all pupil's clothes should carry name tags.
 
-console.log(blanks);
+    Students may also bring, if they desire, an ${answers[11]} OR a ${answers[12]} OR a ${answers[13]}.
 
+PARENTS ARE REMINDED THAT FIRST YEARS
+
+ARE NOT ALLOWED THEIR OWN BROOMSTICK`;
+    document.getElementById('harryPotterPassage').innerHTML = text;
+    console.log(text);
 });
+
+
 
 
 
@@ -24,20 +43,4 @@ console.log(blanks);
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
 
-//     `We are ${blank1} to inform you that you have been accepted at
-//     Hogwarts School of ${blank2} and ${blank3}.
-//     Please find enclosed a list of all necessary books and equipment.
-//     Term begins on 1 September. We await your owl by no later than 31 July.
 
-//     UNIFORM
-//     First-year students will require:
-//     1. Three sets of ${blank4} ${blank5}
-//     2. One ${blank6} ${blank7} ${blank8} for day wear
-//     3. One pair of ${blank9} gloves (${blank10} hide or similar)
-//     Please note that all pupil's clothes should carry name tags.
-
-//     Students may also bring, if they desire, an ${blank11} OR a ${blank12} OR a ${blank13}.`
-
-// PARENTS ARE REMINDED THAT FIRST YEARS
-
-// ARE NOT ALLOWED THEIR OWN BROOMSTICK
