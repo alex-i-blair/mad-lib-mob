8bc37ccd5b1817643c3be5486328fa5d15ea899e
Philippe Ngom <philngom@gmail.com>
2021-10-08 13:47:12 -0700

    moved passage to app.js, relabelled blanks

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 // import functions
 
 // reference needed DOM elements
+const blank1 = document.getElementById('')
 
 
-// set event listeners 
+// set event listeners
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+    `We are ${blank1} to inform you that you have been accepted at
+    Hogwarts School of ${blank2} and ${blank3}.
+    Please find enclosed a list of all necessary books and equipment.
+    Term begins on 1 September. We await your owl by no later than 31 July.
+
+    UNIFORM
+    First-year students will require:
+    1. Three sets of ${blank4} ${blank5}
+    2. One ${blank6} ${blank7} ${blank8} for day wear
+    3. One pair of ${blank9} gloves (${blank10} hide or similar)
+    Please note that all pupil's clothes should carry name tags.
+
+    Students may also bring, if they desire, an ${blank11} OR a ${blank12} OR a ${blank13}.`
+
+PARENTS ARE REMINDED THAT FIRST YEARS
+
+ARE NOT ALLOWED THEIR OWN BROOMSTICK
