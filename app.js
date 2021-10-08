c39f0646ca591810c951960677ed4709943e6e0e
Alex Blair <bclan11@gmail.com>
2021-10-08 14:08:59 -0700

    blanks for looping

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // import functions
 
 // reference needed DOM elements
-const blank1 = document.getElementById('')
+const blanks = [];
+]
+for (let i = 1; i <= 13; i++) {
+    blanks[i] = document.getElementById(`blank-${i}`);
+    console.log(blank1.value);
+}
+
 
 
 // set event listeners
@@ -9,20 +15,20 @@ const blank1 = document.getElementById('')
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
 
-    `We are ${blank1} to inform you that you have been accepted at
-    Hogwarts School of ${blank2} and ${blank3}.
-    Please find enclosed a list of all necessary books and equipment.
-    Term begins on 1 September. We await your owl by no later than 31 July.
+//     `We are ${blank1} to inform you that you have been accepted at
+//     Hogwarts School of ${blank2} and ${blank3}.
+//     Please find enclosed a list of all necessary books and equipment.
+//     Term begins on 1 September. We await your owl by no later than 31 July.
 
-    UNIFORM
-    First-year students will require:
-    1. Three sets of ${blank4} ${blank5}
-    2. One ${blank6} ${blank7} ${blank8} for day wear
-    3. One pair of ${blank9} gloves (${blank10} hide or similar)
-    Please note that all pupil's clothes should carry name tags.
+//     UNIFORM
+//     First-year students will require:
+//     1. Three sets of ${blank4} ${blank5}
+//     2. One ${blank6} ${blank7} ${blank8} for day wear
+//     3. One pair of ${blank9} gloves (${blank10} hide or similar)
+//     Please note that all pupil's clothes should carry name tags.
 
-    Students may also bring, if they desire, an ${blank11} OR a ${blank12} OR a ${blank13}.`
+//     Students may also bring, if they desire, an ${blank11} OR a ${blank12} OR a ${blank13}.`
 
-PARENTS ARE REMINDED THAT FIRST YEARS
+// PARENTS ARE REMINDED THAT FIRST YEARS
 
-ARE NOT ALLOWED THEIR OWN BROOMSTICK
+// ARE NOT ALLOWED THEIR OWN BROOMSTICK
