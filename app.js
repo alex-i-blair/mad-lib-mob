722fc7f85c918df5edf1e0ba3026594d6f45504c
Alex Blair <bclan11@gmail.com>
2021-10-08 16:13:48 -0700

    breaks

--- app.js
+++ app.js
@@ -20,22 +20,22 @@ buttonPress.addEventListener('click', () => {
     message.textContent = 'Congratulations! You are accepted!';
 
     let text = `We are ${answers[1]} to inform you that you have been accepted at
-    Hogwarts School of ${answers[2]} and ${answers[3]}.
-    Please find enclosed a list of all necessary books and equipment.
-    Term begins on 1 September. We await your owl by no later than 31 July.
-
-    UNIFORM
-    First-year students will require:
-    1. Three sets of ${answers[4]} ${answers[5]}
-    2. One ${answers[6]} ${answers[7]} ${answers[8]} for day wear
-    3. One pair of ${answers[9]} gloves (${answers[10]} hide or similar)
-    Please note that all pupil's clothes should carry name tags.
-
-    Students may also bring, if they desire, an ${answers[11]} OR a ${answers[12]} OR a ${answers[13]}.
-
-PARENTS ARE REMINDED THAT FIRST YEARS
-
-ARE NOT ALLOWED THEIR OWN BROOMSTICK`;
+    Hogwarts School of ${answers[2]} and ${answers[3]}.<br>
+    Please find enclosed a list of all necessary books and equipment.<br>
+    Term begins on 1 September. We await your owl by no later than 31 July.<br>
+    <br>
+    UNIFORM<br>
+    First-year students will require:<br>
+    1. Three sets of ${answers[4]} ${answers[5]}<br>
+    2. One ${answers[6]} ${answers[7]} ${answers[8]} for day wear<br>
+    3. One pair of ${answers[9]} gloves (${answers[10]} hide or similar)<br>
+    Please note that all pupil's clothes should carry name tags.<br>
+    <br>
+    Students may also bring, if they desire, an ${answers[11]} OR a ${answers[12]} OR a ${answers[13]}.<br>
+    <br>
+PARENTS ARE REMINDED THAT FIRST YEARS<br>
+<br>
+ARE NOT ALLOWED THEIR OWN BROOMSTICK<br>`;
     document.getElementById('harryPotterPassage').innerHTML = text;
 
 });
